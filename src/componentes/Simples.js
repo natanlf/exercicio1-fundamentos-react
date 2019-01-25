80e0873070e61035ddeec9e89c4dfa8ebaa20a4c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-25 13:59:44 -0200

    Retornando mais de um componente

--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,4 +1,10 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text } from 'react-native'
 
-export default props => <Text> Arrow function: {props.texto}</Text>
+//colocando a View, podemos usar mais de um componente
+
+export default props => 
+<View>
+  <Text> Arrow function 1: {props.texto}</Text>
+  <Text> Arrow function 2: {props.texto}</Text>  
+</View>
