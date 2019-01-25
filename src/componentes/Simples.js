bb65f36af1e4194a8549e09fa78861e13fe65ee0
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-25 14:50:52 -0200

    Criando estilo

--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../estilo/Padrao'
 
-//retornando mais de um componente com array
+//estamos usando o estilo Padrao que importamos e como colocamos dentro de um array, podemos colocar vários estilos
+//Com array [Padrao.ex], mas como não precisamos vamos tirar o array
 
-export default props => 
-[
-  <Text key={1}> Arrow function 1: {props.texto}</Text>,
-  <Text key={2}> Arrow function 2: {props.texto}</Text>  
-]
+export default props => <Text style={Padrao.ex}> Arrow function 1: {props.texto}</Text>
+
