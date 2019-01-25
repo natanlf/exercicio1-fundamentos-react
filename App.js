dfd6ac5c4abb60eba6a2eb10487be83153f97378
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-25 13:17:54 -0200

    Recriando o componente App

--- App.js
+++ App.js
@@ -1,50 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+//esse componente está sendo usado dentro do index.js, por isso precisamos exportar
+export default class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+  //reder() é a função responsável por renderizar a tela
+  render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.f20}>App!</Text>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+//serve para colocarmos estilos nos componentes
+const styles = StyleSheet.create({ //objeto com todos os estilos 
+  container: { //chamei a chave de container e o valor é o objeto que vem em seguida
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  f20: {
+    fontSize: 40
+  }
+})
