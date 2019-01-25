2b8e8dd26eb48e9c71227283f149dc3508d6e16f
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-25 13:54:09 -0200

    Usando Arrow function

--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,7 +1,4 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-//componente baseado em função, vamos importar esse componente no app.js
-export default function(props) { //parametro que é enviado pelo App.js, assim vamos acessar as propriedades do componente App.js
-    return <Text>{props.texto}</Text> //props é o nome do parametro que recebi mas o nome da propriedade que defini em app.js foi texto
-}
+export default props => <Text> Arrow function: {props.texto}</Text>
